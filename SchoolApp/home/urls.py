a29fe77f7546f258b346430cfe886a79aacd2463
Ceres445 <Ceres445@users.noreply.github.com>
2021-06-10 17:54:16 +0530

    added user creation

--- SchoolApp/home/urls.py
+++ SchoolApp/home/urls.py
@@ -6,4 +6,5 @@ import home.views as views
 urlpatterns = [
     url(r'^$', views.index, name="home_page"),
     url(r"^accounts/", include("django.contrib.auth.urls")),
+    url(r"^register/", views.register, name="register")
 ]
