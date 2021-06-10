532673d9c2d8b02bf247eda11299a727e2d35348
Ceres <ddruva.boomer@gmail.com>
2021-06-10 16:52:24 +0530

    removed a password validator

--- SchoolApp/SchoolApp/settings.py
+++ SchoolApp/SchoolApp/settings.py
@@ -92,9 +92,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
