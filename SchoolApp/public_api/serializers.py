d4272ebcb7f52fd6ba80df3bfae1b5ea54b9f193
Ceres445 <Ceres445@users.noreply.github.com>
2021-06-29 23:10:21 +0530

    added api app

---
+++ SchoolApp/public_api/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Post, Comment, Category
+
+
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'posts']
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=False)
+
+    class Meta:
+        model = Post
+        depth = 1
+        fields = ('id', 'title', 'content', 'author', 'created_at', 'comments', 'category')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'body', 'author', 'post']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'posts']
