c44a2f593d6c9351b811e1c91a8ae5c760404006
Ceres <ddruva.boomer@gmail.com>
2021-07-02 23:45:42 +0530

    make and use profile models and views;

--- SchoolApp/SchoolApp/urls.py
+++ SchoolApp/SchoolApp/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from SchoolApp import settings
 
 urlpatterns = [
     url(r"^", include("home.urls")),
     path('admin/', admin.site.urls),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
