0410993d7be19e1ee4a28b763d45637e56f96955
Ceres <ddruva.boomer@gmail.com>
2021-07-05 13:16:04 +0530

    added compression

---
+++ SchoolApp/staticfiles/CACHE/js/output.fd63e5ac2c05.js
@@ -0,0 +1,6 @@
+const tabcontent=document.querySelector('.tabcontent');const tablinks=document.getElementsByClassName('tablink');for(const i of tablinks){i.addEventListener('click',async function(){const data=await fetch(window.location.href+`api/posts?category=${this.name}`);const json=await data.json();tabcontent.innerHTML='';json.forEach(el=>{const html=`<div class='post'>
+													<h1>${el.title}</h1>
+													<p>${el.content}</p>
+													<div class = 'footer'>${el.author}</div>
+											</div>`;tabcontent.insertAdjacentHTML('beforeend',html);});});}
+document.getElementById('defaultOpen').click();;
