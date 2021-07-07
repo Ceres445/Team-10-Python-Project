67beba3a4b4eddbdcdb57eb4bee615c458c86069
Ceres <ddruva.boomer@gmail.com>
2021-07-07 19:11:12 +0530

    fixed wrong model

--- SchoolApp/public_api/serializers.py
+++ SchoolApp/public_api/serializers.py
@@ -36,7 +36,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
     posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    key_class = serializers.StringRelatedField(many=False, required=False, queryset=Classes.objects.all(),
+    key_class = serializers.PrimaryKeyRelatedField(many=False, required=False, queryset=Classes.objects.all(),
                                                allow_null=True)
 
     class Meta:
