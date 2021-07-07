29b84f5f3afc18897c6539603829d168bc5b51e1
Ceres <ddruva.boomer@gmail.com>
2021-07-07 18:51:41 +0530

    added permissions to models

--- SchoolApp/public_api/permissions.py
+++ SchoolApp/public_api/permissions.py
@@ -1,9 +1,29 @@
 from rest_framework import permissions
 
+from public_api.models import Post
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.owner == request.user
+        return obj.author == request.user
+
+
+class IsInClass(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        print(obj, request.user)
+        if not isinstance(obj, Post):
+            try:
+                obj = obj.post
+            except AttributeError:
+                raise RuntimeError("Invalid object of type", obj.__type__, 'passed, IsInClass expects Post or Comment')
+        if obj.category.name != 'Class':
+            return True
+        if request.method in permissions.SAFE_METHODS:
+            if obj.category.key_class in request.user.profile.courses.all():
+                return True
+            else:
+                return False
+        return obj.author == request.user
