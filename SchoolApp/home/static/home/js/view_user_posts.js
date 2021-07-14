8d7e793683637cb579568f2117cf0b2acd14c3b9
Ceres <ddruva.boomer@gmail.com>
2021-07-14 19:01:16 +0530

    fixed user posts

--- SchoolApp/home/static/home/js/view_user_posts.js
+++ SchoolApp/home/static/home/js/view_user_posts.js
@@ -8,6 +8,10 @@ const container = document.querySelector('.posts');
 	const postsData = await fetch(
 		window.location.origin + `/api/posts?author=${user_pk}`
 	);
+	if (postsData.status === 404) {
+		container.innerHTML = 'Not found'; // TODO: show error
+		return;
+	}
 	const postsJson = await postsData.json();
 	postsJson.forEach(el => addPostToHTML(el, container));
 })();
