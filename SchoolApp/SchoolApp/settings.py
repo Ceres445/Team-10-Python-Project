68f3fa8f529eee03d8cb4d31f2bcd258637edca4
Ceres445 <Ceres445@users.noreply.github.com>
2021-07-15 13:25:49 +0530

    added posting

--- SchoolApp/SchoolApp/settings.py
+++ SchoolApp/SchoolApp/settings.py
@@ -140,3 +140,9 @@ EMAIL_PORT = 587
 # media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
+    ]
+}
