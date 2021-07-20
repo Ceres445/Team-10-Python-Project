f4bb15c52b7cbaef2641d27fe2f5a51f21219262
Ceres <ddruva.boomer@gmail.com>
2021-07-20 23:47:28 +0530

    change css to dark mode

--- SchoolApp/home/urls.py
+++ SchoolApp/home/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("profile/edit", views.edit_profile, name="editProfile"),
     path("profile/", views.view_profile, name="profile"),
     path("profile/posts", views.view_user_posts, name='viewPosts'),
+    path("profile/<str:username>", views.view_profile, name="profileView"),
     path('avatar_change/', views.change_avatar, name="changeAvatar"),
     path('post/<int:pk>/', views.view_post_detail, name="postDetail")
 ]
