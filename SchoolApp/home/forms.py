3a15baab98860eac82d58ce26b0330d25efa05ea
Ceres445 <Ceres445@users.noreply.github.com>
2021-07-22 13:56:08 +0530

    added form for new css

--- SchoolApp/home/forms.py
+++ SchoolApp/home/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 from home.models import Profile
@@ -13,4 +14,16 @@ class CustomUserCreationForm(UserCreationForm):
 class AvatarChangeForm(ModelForm):
     class Meta:
         model = Profile
-        fields = ['bio', 'avatar']
+        fields = ['bio']
+
+
+class CustomUserChangeForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+        )
