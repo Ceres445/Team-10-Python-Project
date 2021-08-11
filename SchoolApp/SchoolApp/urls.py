ec435cfa410acd063aad2166881a44314f2d88b6
Ceres <ddruva.boomer@gmail.com>
2021-08-11 23:34:17 +0530

    Basically did everything Added class detail and list views for logged in user Added assignments upload by teachers Added uploading answers for students Added answer review for teachers

--- SchoolApp/SchoolApp/urls.py
+++ SchoolApp/SchoolApp/urls.py
@@ -22,6 +22,7 @@ from SchoolApp import settings
 
 urlpatterns = [
     path('', include("home.urls")),
+    path('classes/', include("classes.urls")),
     path('admin/', admin.site.urls),
     path('api/', include('public_api.urls')),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ SchoolApp/classes/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+
+from classes.models import Assignment, Upload
+
+
+class AssignmentCreationForm(ModelForm):
+    class Meta:
+        model = Assignment
+        fields = ['key_class', 'title', 'questions', 'ends_at']
+
+
+class UploadCreationForm(ModelForm):
+    class Meta:
+        model = Upload
+        fields = ['assignment', 'file']
