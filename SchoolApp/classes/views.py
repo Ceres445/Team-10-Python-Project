d28ea805e60fd3400858cabe42e90cfcc26d839f
Ceres445 <Ceres445@users.noreply.github.com>
2021-08-11 23:46:35 +0530

    cleaned up code for review Code needs to be reviewd, messy logic used

--- SchoolApp/classes/views.py
+++ SchoolApp/classes/views.py
@@ -11,14 +11,16 @@ from home.models import Classes
 
 
 @login_required
-def assignment_creation(request):
+def assignment_creation(request, pk):
     """Form to create assignment"""
+    class_object = get_object_or_404(Classes, id=pk)
     classes_thought = Classes.objects.all().filter(teacher_id=request.user)
     if request.method == 'POST':
         form = AssignmentCreationForm(request.POST, request.FILES)
         if form.is_valid():
+            form.instance.key_class = class_object
             form.save()
-            return redirect(reverse('classesView'))
+            return redirect(reverse('classesDetail', args=[pk]))
     else:
         form = AssignmentCreationForm()
 
@@ -38,7 +40,7 @@ def classes_view(request):
 def classes_detail(request, pk=1):
     class_object = get_object_or_404(Classes, id=pk)
     if class_object in request.user.profile.courses.all() or class_object.teacher_id == request.user:
-        assignments = Assignment.objects.all().filter(key_class=class_object)
+        assignments = Assignment.objects.all().filter(key_class=class_object)[::-1]
         teacher = class_object.teacher_id == request.user
         return render(request, 'classes/classes_detail.html', {'class': class_object,
                                                                'assignments': assignments,
@@ -57,7 +59,7 @@ def assignment_submit(request, pk=1):
             if form.is_valid():
                 form.instance.author = request.user
                 form.save()
-                return redirect(reverse('homePage'))
+                return redirect(reverse('classesDetail', args=[pk]))
         else:
             form = UploadCreationForm()
 
@@ -72,7 +74,7 @@ def view_submissions(request, pk):
 
     assignment = get_object_or_404(Assignment, pk=pk)
     if assignment.key_class.teacher_id == request.user:
-        uploads = Upload.objects.all().filter(assignment=assignment)
+        uploads = Upload.objects.all().filter(assignment=assignment)[::-1]
         return render(request, 'classes/view_submissions.html', {'uploads': uploads})
     else:
         raise PermissionDenied("Only the teacher can see uploads")
