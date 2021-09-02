14517ef83ccb4bd840ab1c841012c0188d7dbeb4
Ceres <Ceres445@users.noreply.github.com>
2021-09-02 17:03:36 +0530

    removed old tests

--- SchoolApp/home/tests.py
+++ SchoolApp/home/tests.py
@@ -1,12 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-<<<<<<< HEAD
-from django.contrib.staticfiles import finders
-
-result = finders.find('registration/login.css')
-searched_locations = finders.searched_locations
-
-print(result, searched_locations)
-=======
->>>>>>> master
