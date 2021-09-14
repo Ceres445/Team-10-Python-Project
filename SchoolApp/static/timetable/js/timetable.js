1679b9f81e9f97a80600e62b3f94655dbad4f5c9
Ceres <Ceres445@users.noreply.github.com>
2021-09-14 23:01:46 +0530

    added table

--- SchoolApp/static/timetable/js/timetable.js
+++ SchoolApp/static/timetable/js/timetable.js
@@ -1,16 +1,40 @@
 const records = JSON.parse(document.getElementById('records').textContent);
 
-const table = document.getElementById('table');
-for (const record of records) {
-	table.insertAdjacentHTML(
-		'beforeend',
-		`<tr>
-        <td>${record.day}</td>
-        <td>${record.subject}</td>
-        <td>${record.time}</td>
-        <td><a href="${record.link}">Link</a></td>
-    </tr>
-  `
-	);
+const table_element = document.getElementById('table');
+function render_html(map, field) {
+	for (const [grouped_field, list] of map) {
+		list.forEach((record, i) => {
+			let string = '';
+			for (const key in record) {
+				if (Object.prototype.hasOwnProperty.call(record, key))
+					if (key === 'link')
+						string += `<td><a href="${record[key]}">Link</a> </td>`;
+					else string += `<td>${record[key]}</td>`;
+			}
+			const row = table_element.insertRow(table_element.rows.length);
+			if (i === 0) {
+				row.innerHTML = `
+               <td rowspan="${list.length}" class="span">${grouped_field}</td>
+							${string}
+				`;
+			} else row.innerHTML = string;
+		});
+	}
 }
+function groupby(list, field) {
+	const map = new Map();
+	list.forEach(item => {
+		const key = item[field];
+		const collection = map.get(key);
+		delete item[field];
+		if (!collection) {
+			map.set(key, [item]);
+		} else {
+			collection.push(item);
+		}
+	});
+	return map;
+}
+const grouped = groupby(records, 'day');
+render_html(grouped, 'day');
 // TODO: Add sorting/ filtering
