11465e33e085c7db02b75572b26c898366e0be97
Ceres <Ceres445@users.noreply.github.com>
2021-09-29 13:29:32 +0530

    Added filtering and improved html

--- SchoolApp/static/timetable/js/timetable.js
+++ SchoolApp/static/timetable/js/timetable.js
@@ -3,16 +3,21 @@ const records = JSON.parse(document.getElementById('records').textContent);
 const table_element = document.getElementById('table');
 const table_body = table_element.getElementsByTagName('tbody')[0];
 const table_head = table_element.getElementsByTagName('thead')[0];
+const filterby_checklist = document.querySelector('.filterby-checklists');
+const filterby_support = document.querySelector('.filterby-support');
+
 const order = {
 	day: ['day', 'subject', 'time', 'link'],
 	subject: ['subject', 'day', 'time', 'link'],
 };
-
-function render_html(map, field) {
-	table_head.innerHTML = '';
+let _groupby = 'day'; // Default filter and group by day
+let _filterby = 'day';
+function render_html() {
+	const map = groupby(filterby(records)); // get the filtered and grouped map
+	table_head.innerHTML = ''; // Rewrite the table headers
 	table_head.insertRow(0).innerHTML = `<td>
-${order[field].map(capitalize).join('</td><td>')}</td>`;
-	table_body.innerHTML = '';
+${order[_groupby].map(capitalize).join('</td><td>')}</td>`;
+	table_body.innerHTML = ''; // Rewrite the table body with the map
 	for (const [grouped_field, list] of map) {
 		list.forEach((record, i) => {
 			let string = '';
@@ -32,13 +37,27 @@ ${order[field].map(capitalize).join('</td><td>')}</td>`;
 		});
 	}
 }
+function filterby(records) {
+	// get filters and filter our records
+	const filters = document.getElementsByClassName('filterby');
+	if (filters.length !== 0) {
+		const checked = [];
+		for (const i of filters) {
+			if (i.checked) {
+				checked.push(i.name);
+			}
+		}
+		return records.filter(el => checked.includes(el[_filterby]));
+	} else return records;
+}
 
-function groupby(list, field) {
+function groupby(list) {
+	// get group field and group our records
 	const map = new Map();
 	list.forEach(item => {
-		const key = item[field];
+		const key = item[_groupby];
 		const collection = map.get(key);
-		const { [field]: _, ...test } = item;
+		const { [_groupby]: _, ...test } = item;
 		if (!collection) {
 			map.set(key, [test]);
 		} else {
@@ -48,17 +67,79 @@ function groupby(list, field) {
 	return map;
 }
 
-const grouped = groupby(records, 'day');
-render_html(grouped, 'day');
-
+Array.from(document.getElementsByClassName('filterbutton')).forEach(el =>
+	el.addEventListener('click', () => {
+		_filterby = el.name;
+		filterby_checklist.innerHTML = '';
+		[...new Set(records.map(el => el[_filterby]))].forEach(el => {
+			filterby_checklist.insertAdjacentHTML(
+				'afterbegin',
+				`<label class="checkbox">
+									<input type="checkbox" class="filterby load" name="${el}">${el}
+								</label>`
+			);
+		});
+		mount_handler();
+		filterby_support.classList.remove('hide');
+	})
+);
+// event listeners for groupby buttons
 Array.from(document.getElementsByClassName('groupby')).forEach(el =>
 	el.addEventListener('click', () => {
-		render_html(groupby(records, el.name), el.name);
+		_groupby = el.name;
 	})
 );
 
-// TODO: Add sorting/ filtering
+// Event listeners for filterby buttons that give the menus
+Array.from(document.getElementsByClassName('filterby-support-button')).forEach(
+	el =>
+		el.addEventListener('click', () => {
+			switch (el.name) {
+				case 'all':
+					document
+						.querySelectorAll('.filterby')
+						.forEach(el => (el.checked = true));
+					render_html();
+					break;
+				case 'clear':
+					document
+						.querySelectorAll('.filterby')
+						.forEach(el => (el.checked = false));
+					render_html();
+					break;
+				case 'remove':
+					document
+						.querySelectorAll('.filterby')
+						.forEach(el => (el.checked = false));
+					filterby_support.classList.add('hide');
+					filterby_checklist.innerHTML = '';
+					render_html();
+					break;
+			}
+		})
+);
+
+// mount render_html onto all buttons and inputs
+function mount_handler() {
+	document.querySelectorAll('.load').forEach(el => {
+		if (el.hasAttribute('listener')) {
+			return;
+		}
+		switch (el.tagName) {
+			case 'INPUT':
+				el.setAttribute('listener', 'true');
+				el.addEventListener('change', () => render_html());
+				break;
+			case 'BUTTON':
+				el.setAttribute('listener', 'true');
+				el.addEventListener('click', () => render_html());
+		}
+	});
+}
+// TODO: Add sorting
 
 function capitalize(s) {
 	return s[0].toUpperCase() + s.slice(1);
 }
+
+render_html(); // load html in the begin
