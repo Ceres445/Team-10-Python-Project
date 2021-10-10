a489be4fca06079f0c805b30c593d38577d61d35
Ceres <Ceres445@users.noreply.github.com>
2021-10-10 16:51:28 +0530

    Added some description and new templatetag to handle urls for static files

--- SchoolApp/apps/classes/templatetags/tags.py
+++ SchoolApp/apps/classes/templatetags/tags.py
@@ -1,8 +1,23 @@
 from django import template
+from django.templatetags import static
 
 register = template.Library()
 
 
+# used in classes_view.html
 @register.filter
 def length_active(assignments):
     return len([1 for x in assignments if x.is_active()])
+
+
+# COPIED FROM https://stackoverflow.com/a/47336360/12451222
+class FullStaticNode(static.StaticNode):
+    def url(self, context):
+        request = context['request']
+        return request.build_absolute_uri(super().url(context))
+
+
+# used in base.html
+@register.tag('fullstatic')
+def do_static(parser, token):
+    return FullStaticNode.handle_token(parser, token)
