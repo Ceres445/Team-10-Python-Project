f3f53f7b39b5dfdd9f28c23bd969f05408fce671
Ceres <42400528+Ceres445@users.noreply.github.com>
2022-02-02 21:59:44 +0530

    fixed import

--- SchoolApp/SchoolApp/settings.py
+++ SchoolApp/SchoolApp/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
